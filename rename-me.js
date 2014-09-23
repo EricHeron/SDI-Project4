02f616df1bda7610dd7cbb244fd0b05923446c5f
Eric Heron <echeron@fullsail.edu>
2014-09-23 17:45:47 -0400

    function 1

--- rename-me.js
+++ rename-me.js
@@ -1,1 +1,18 @@
-alert("JavaScript works!");
+// Eric Heron
+// SDI Term: 1409
+// Section: 02
+// Project 4
+
+
+// Format a number to use a specific number of decimal places, as for money:
+function myDecimal(){
+	var num = 2.1;
+	var n = num.toFixed(2);
+	console.log(n);
+}
+myDecimal();
+
+
+
+
+
